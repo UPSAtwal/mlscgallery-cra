aa5c1094555e8db0adc0dc3bb99a49ed68f454d2
upsatwal <UPSAtwal@users.noreply.github.com>
2023-06-30 17:51:21 +0530

    remove routing and only show gallery page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
+// import './App.css';
 import Gallery from "./pages/gallery/gallery";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/gallery" element={<Gallery />} />
-      </Routes>
-    </Router>
-
+  <>
+  <Gallery/>
+  </>
   );
 }
 
