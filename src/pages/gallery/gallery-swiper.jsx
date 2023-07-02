b8b99c579c82e75d64b12980d73ffb39d249376a
upsatwal <UPSAtwal@users.noreply.github.com>
2023-07-02 16:19:09 +0530

    prototype galler swiper works

--- src/pages/gallery/gallery-swiper.jsx
+++ src/pages/gallery/gallery-swiper.jsx
@@ -2,33 +2,53 @@
 //i will fix it after 5. And i am ready to bet that the gallery page will be the best page of the whole website
 
 import React from 'react';
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
 import './gallery-swiper.css';
 
 // Import your images dynamically
+
 function importAll(r) {
     return r.keys().map(r);
 }
 
-const images = importAll(require.context('./assets/gallery-images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('./gallery-images/', false, /\.(png|jpe?g|svg|webp)$/));
 
-SwiperCore.use([Navigation, Pagination]);
-
-function Gallery() {
+function GallerySwiper() {
     return (
-        <div className="gallery-container">
-            <h1>Gallery</h1>
-            <Swiper navigation pagination>
+        <div className='gallery-container'>
+            <Swiper
+                style={{
+                    '--swiper-navigation-color': '#fff',
+                    '--swiper-pagination-color': '#fff',
+                }}
+                loop={true}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation={true}
+                modules={[Navigation, Pagination]}
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true,
+                }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
                 {images.map((image, index) => (
                     <SwiperSlide key={index}>
-                        <img src={image.default} alt={`${index + 1}`} />
+                        <img key={index} src={image} alt={`MLSC Team ${index + 1}`}></img>
                     </SwiperSlide>
                 ))}
             </Swiper>
         </div>
     );
-}
+};
 
-export default Gallery;
+
+export default GallerySwiper;
